a91ebe48333dc1616ac5a25e357798dc4c080fd9
Raj Bohara <raj.bohara@kadellabs.com>
2025-11-10 15:43:16 +0530

    removed settings

--- src/app/(modules)/settings/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SettingsLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return (
-    <div>{children}</div>
-  )
-}
-
-export default SettingsLayout
